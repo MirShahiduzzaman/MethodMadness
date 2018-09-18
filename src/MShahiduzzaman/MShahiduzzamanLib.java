1f5351cb88a9189284ad95c7374a194929cc48f4
MirShahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-09-18 10:14:38 -0400

    Updates

--- src/MShahiduzzaman/MShahiduzzamanLib.java
+++ src/MShahiduzzaman/MShahiduzzamanLib.java
@@ -6,4 +6,46 @@ public class MShahiduzzamanLib
     {
         System.out.println(str);
     }
+
+    public static boolean isPalindrome(String word)
+    {
+        String check = "";
+
+        for(int i = (word.length()-1);i>=0;i--)
+        {
+            check += word.substring(i,i+1);
+        }
+
+        if(check.equals(word))
+        {
+            return(true);
+        }
+        else
+        {
+            return(false);
+        }
+    }
+
+    public static String dateStr(String date)
+    {
+        int index = date.indexOf("/");
+
+        while(index>=0)
+        {
+            date = date.substring(0,index) + "-" + date.substring((index+1));
+            index = date.indexOf("/");
+        }
+
+        return(date);
+    }
+
+    public static int sumUpTo(int num)
+    {
+        int total = 0;
+        for(int i = 1; i<=num; i++)
+        {
+            total += i;
+        }
+        return(total);
+    }
 }
--- src/MShahiduzzaman/TestFile.java
+++ src/MShahiduzzaman/TestFile.java
@@ -1,11 +1,18 @@
 package MShahiduzzaman;
 
 import static MShahiduzzaman.MShahiduzzamanLib.println;
+import static MShahiduzzaman.MShahiduzzamanLib.isPalindrome;
+import static MShahiduzzaman.MShahiduzzamanLib.dateStr;
+import static MShahiduzzaman.MShahiduzzamanLib.sumUpTo;
 
 public class TestFile
 {
     public static void main(String[] args)
     {
         println("I love a cup of Java in the morning");
+
+        System.out.println(isPalindrome("mom"));
+        System.out.println(dateStr("12/20/01"));
+        System.out.println(sumUpTo(2));
     }
 }
