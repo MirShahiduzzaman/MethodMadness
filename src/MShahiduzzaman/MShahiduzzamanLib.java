f2a41c63fcb6a3fb6f5cd3b2b5f622ab8ffcbd4e
BT_1N3_09@bktech.local <BT_1N3_09@bktech.local>
2018-09-18 09:17:18 -0400

    Initial commit

---
+++ src/MShahiduzzaman/MShahiduzzamanLib.java
@@ -0,0 +1,9 @@
+package MShahiduzzaman;
+
+public class MShahiduzzamanLib
+{
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+}
---
+++ src/MShahiduzzaman/TestFile.java
@@ -0,0 +1,11 @@
+package MShahiduzzaman;
+
+import static MShahiduzzaman.MShahiduzzamanLib.println;
+
+public class TestFile
+{
+    public static void main(String[] args)
+    {
+        println("I love a cup of Java in the morning");
+    }
+}
